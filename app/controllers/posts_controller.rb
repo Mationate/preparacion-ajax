aaab82d16f189ffcbd892ccdb0cde600fe182fda
Matias Oñate <mationate.0@gmail.com>
2019-03-01 11:56:13 -0300

    create functionality in posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,9 +1,25 @@
 class PostsController < ApplicationController
+  before_action :post_params, only: []
   def index
+    @posts = Post.order('id DESC')
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    @post.user = current_user
+    if @post.save
+      respond_to :js
+    else
+      redirect_to root_path, alert: 'Error al crear el post, intente nuevamente'
+    end
     
   end
-  def create
-    
+  
+  private 
+
+  def post_params
+    params.require(:post).permit(:title, :content)
   end
   
   

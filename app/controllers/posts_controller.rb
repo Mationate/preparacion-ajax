b300584dd085144830a11a9ec1bf4db370594928
Matias Oñate <mationate.0@gmail.com>
2019-03-01 12:42:16 -0300

    destroy method functionality

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,6 @@
 class PostsController < ApplicationController
-  before_action :post_params, only: []
+
+  before_action :set_post, only: [:destroy]
   def index
     @posts = Post.order('id DESC')
     @post = Post.new
@@ -13,14 +14,20 @@ class PostsController < ApplicationController
     else
       redirect_to root_path, alert: 'Error al crear el post, intente nuevamente'
     end
-    
   end
-  
-  private 
+
+  def destroy
+    @post.destroy
+    respond_to :js
+  end
+
+  private
+
+  def set_post
+    @post = Post.find(params[:id])
+  end
 
   def post_params
     params.require(:post).permit(:title, :content)
   end
-  
-  
 end
